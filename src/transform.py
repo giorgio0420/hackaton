3a770ad05895bd707e9e8267e9cc0fc4a27665af
Giorgio De Santis <desantis.1921727@studenti.uniroma1.it>
2025-05-27 08:53:39 +0200

    Update transform.py

--- src/transform.py
+++ src/transform.py
@@ -1,5 +1,5 @@
 import torch
-from torch_geometric.utils import degree
+from torch_geometric.utils import degree, clustering
 
 def add_node_degree_feature(data):
     row, col = data.edge_index
@@ -20,4 +20,17 @@ def add_node_degree_feature(data):
         data.x = deg.float()
 
     return data
+
+def add_clustering_coefficient(data):
+    cc = clustering(data.edge_index, num_nodes=data.num_nodes).unsqueeze(1)
+
+    if data.x is not None and data.x.numel() > 0:
+        if data.x.dim() == 1:
+            data.x = data.x.unsqueeze(1)
+        data.x = torch.cat([data.x.float(), cc.float()], dim=1)
+    else:
+        data.x = cc.float()
+
+    return data
+
 
